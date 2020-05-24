e69dee34c4cb11b47f0595cbea68399b65ec5856
natan-git <54308928+natan-git@users.noreply.github.com>
2020-05-24 11:58:16 +0300

    fix bug

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('public'));
 } else {
   var allowCrossDomain = function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://hidden-refuge-74609.herokuapp.com/');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
