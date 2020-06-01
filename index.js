cfda6a0c0028e181bd5b5d4286250f5d95554de8
natan-git <54308928+natan-git@users.noreply.github.com>
2020-06-01 10:42:48 +0300

    dep fix5

--- index.js
+++ index.js
@@ -26,17 +26,17 @@ app.use(
   })
 );
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve(__dirname, 'public')));
-} else {
-  var allowCrossDomain = function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  };
-  app.use(allowCrossDomain);
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.resolve(__dirname, 'public')));
+// } else {
+//   var allowCrossDomain = function (req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type');
+//     next();
+//   };
+//   app.use(allowCrossDomain);
+// }
 
 noteRoutes(app);
 
