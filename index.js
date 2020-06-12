fde42795b78023f952a1bce8931063e69a9a27ef
natan-git <54308928+natan-git@users.noreply.github.com>
2020-06-12 11:24:20 +0300

    ss

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ var cookieParser = require('cookie-parser');
 const session = require('express-session');
 const passport = require('passport');
 const noteRoutes = require('./routes/noteRoutes');
+const cors = require('cors');
 
 const app = express();
 
-// app.use(express.static('public'));
+app.use(express.static('public'));
 app.use(express.json());
 app.use(bodyParser.json()); // create the req.body object - from json
 app.use(bodyParser.urlencoded({ extended: false })); // create the req.body object
@@ -26,19 +27,26 @@ app.use(
   })
 );
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('public'));
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true
+};
 
-  // app.use(express.static(path.resolve(__dirname, 'public')));
-} else {
-  var allowCrossDomain = function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  };
-  app.use(allowCrossDomain);
-}
+app.use(cors(corsOptions));
+
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('public'));
+
+//   // app.use(express.static(path.resolve(__dirname, 'public')));
+// } else {
+//   var allowCrossDomain = function (req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type');
+//     next();
+//   };
+//   app.use(allowCrossDomain);
+// }
 
 noteRoutes(app);
 
