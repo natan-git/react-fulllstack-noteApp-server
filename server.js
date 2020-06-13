1e78bb9a1ff5506e5e663ee94537ce373d63bbf2
natan-git <54308928+natan-git@users.noreply.github.com>
2020-06-13 20:50:21 +0300

    fixbug54saascccc

--- server.js
+++ server.js
@@ -26,32 +26,34 @@ app.use(
   })
 );
 
-
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.resolve(__dirname, 'public')));
-} else {
-    var allowCrossDomain = function (req, res, next) {
+  var allowCrossDomain = function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
   };
   app.use(allowCrossDomain);
-
+} else {
   const corsOptions = {
-      origin: ['https://hidden-refuge-74609.herokuapp.com','http://localhost:5000','http://127.0.0.1:5000', 'http://localhost:5000', 'http://127.0.0.1:3000', 'http://localhost:3000'],
-      credentials: true
+    origin: [
+      'https://hidden-refuge-74609.herokuapp.com',
+      'http://localhost:5000',
+      'http://127.0.0.1:5000',
+      'http://localhost:5000',
+      'http://127.0.0.1:3000',
+      'http://localhost:3000',
+    ],
+    credentials: true,
   };
   app.use(cors(corsOptions));
 }
-
 
 // const corsOptions = {
 //   origin: 'http://localhost:3000',
 //   credentials: true
 // };
-
-
 
 // if (process.env.NODE_ENV === 'production') {
 //   app.use(express.static('public'));
@@ -68,9 +70,9 @@ if (process.env.NODE_ENV === 'production') {
 // }
 
 noteRoutes(app);
-app.get('/*',function(req,res){
-  res.sendFile(path.resolve(__dirname,'public/index.html'))
-})
+app.get('/*', function (req, res) {
+  res.sendFile(path.resolve(__dirname, 'public/index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 
