d81f2b0d6a3861a507bcb8871e3dd5aa047687b5
natan-git <54308928+natan-git@users.noreply.github.com>
2020-06-15 14:08:20 +0300

    fixs4324

--- server.js
+++ server.js
@@ -28,13 +28,13 @@ app.use(
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.resolve(__dirname, 'public')));
-  var allowCrossDomain = function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  };
-  app.use(allowCrossDomain);
+  // var allowCrossDomain = function (req, res, next) {
+  //   res.header('Access-Control-Allow-Origin', '*');
+  //   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  //   res.header('Access-Control-Allow-Headers', 'Content-Type');
+  //   next();
+  // };
+  // app.use(allowCrossDomain);
 } else {
   var allowCrossDomain = function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
--- services/dbService.js
+++ services/dbService.js
@@ -1,35 +1,36 @@
 const MongoClient = require('mongodb').MongoClient;
 
 // Connection URL
-const url = (false && process.env.NODE_ENV === 'production') ?
-    'mongodb+srv://natan:skate4life@cluster0-u9ykt.mongodb.net/test?retryWrites=true&w=majority'
-    : 'mongodb://localhost:27017';
-
-const dbName = 'note_db';
+// const url =
+//   false && process.env.NODE_ENV === 'production'
+//     ? 'mongodb+srv://natan:skate4life@cluster0-u9ykt.mongodb.net/test?retryWrites=true&w=majority'
+//     : 'mongodb://localhost:27017';
+const url='mongodb+srv://natan:skate4life@cluster0-u9ykt.mongodb.net/test?retryWrites=true&w=majority';
+const dbName = 'note_db'; 
 var dbConn = null;
 
 async function connect() {
-    if (dbConn) return dbConn;
-    try {
-        const client = await MongoClient.connect(url, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        const db = client.db(dbName);
-        dbConn = db;
-        return db;
-    } catch (err) {
-        console.log('Cannot Connect to DB', err)
-        throw err;
-    }
+  if (dbConn) return dbConn;
+  try {
+    const client = await MongoClient.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    const db = client.db(dbName);
+    dbConn = db;
+    return db;
+  } catch (err) {
+    console.log('Cannot Connect to DB', err);
+    throw err;
+  }
 }
 
 async function getCollection(collectionName) {
-    const db = await connect()
-    return db.collection(collectionName);
+  const db = await connect();
+  return db.collection(collectionName);
 }
 
 module.exports = {
-    // connect,
-    getCollection
-}
+  // connect,
+  getCollection,
+};
