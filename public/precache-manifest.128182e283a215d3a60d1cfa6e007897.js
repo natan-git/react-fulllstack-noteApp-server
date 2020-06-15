908490c859b43765f79849cfc1f68981f6420b7e
natan-git <54308928+natan-git@users.noreply.github.com>
2020-06-15 14:17:45 +0300

    fixsg

---
+++ public/precache-manifest.128182e283a215d3a60d1cfa6e007897.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "512e72f5c7306116629b353e2aaaf51e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25637de167d335dc97d8",
+    "url": "/static/css/main.d4a10ded.chunk.css"
+  },
+  {
+    "revision": "2f6c9eee95147fe4422d",
+    "url": "/static/js/2.da233603.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.da233603.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25637de167d335dc97d8",
+    "url": "/static/js/main.a008e3ab.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  },
+  {
+    "revision": "011c0bd977efd43a727bc8ac4dd6e2be",
+    "url": "/static/media/back.011c0bd9.svg"
+  },
+  {
+    "revision": "e45a3d0d8de698bfbea8ddc00225fd8e",
+    "url": "/static/media/bg-pattern-home-1.e45a3d0d.svg"
+  },
+  {
+    "revision": "2fd804c4ec4e139d392e43c599bd277b",
+    "url": "/static/media/bg-pattern-home-2.2fd804c4.svg"
+  },
+  {
+    "revision": "50b1c575711840c0bafbdd8ae9fde584",
+    "url": "/static/media/delete.50b1c575.svg"
+  },
+  {
+    "revision": "2dc042eab7de6f612d18720b422ad1b5",
+    "url": "/static/media/edit.2dc042ea.svg"
+  },
+  {
+    "revision": "8565e7516f1f311506325ca15e5f911d",
+    "url": "/static/media/painting.8565e751.svg"
+  }
+]);
